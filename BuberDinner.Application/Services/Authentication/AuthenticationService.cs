a7ab621d6bd07a01327c1b861cb618c8ac2a3d79
ghostNoté <selcukbilgen@hotmail.com>
2023-08-23 01:03:43 +0300

    fake authentication service added.

---
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿namespace BuberDinner.Application.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    public AuthenticationResult Login(string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+}
