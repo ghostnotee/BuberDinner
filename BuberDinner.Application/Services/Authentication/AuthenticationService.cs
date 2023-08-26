b2428713deee769a3cb8eff1c5fa969dce08960f
ghostNoté <selcukbilgen@hotmail.com>
2023-08-26 12:35:46 +0300

    sample exception middleware

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -18,15 +18,12 @@ public class AuthenticationService : IAuthenticationService
     public AuthenticationResult Login(string email, string password)
     {
         // 1. Validate the user exists.
-        if (_userRepository.GetUserByEmail(email) is not User user)
-            throw new Exception("User with given email does not exist.");
+        if (_userRepository.GetUserByEmail(email) is not User user) throw new Exception("User with given email does not exist.");
 
 
         // 2. Validate the password is correct  
-        if (user.Password != password)
-        {
-            throw new Exception("Invalid password.");
-        }
+        if (user.Password != password) throw new Exception("Invalid password.");
+
 
         // 3. Create JWT Token
         var token = _jwtTokenGenerator.GenerateToken(user);
