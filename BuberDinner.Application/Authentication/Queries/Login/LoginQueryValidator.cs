3ce74ef17411ac73aeecde66cf4f1c82dbacfa4e
ghostNoté <selcukbilgen@hotmail.com>
2023-08-28 01:27:36 +0300

    error response fixed

---
+++ BuberDinner.Application/Authentication/Queries/Login/LoginQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace BuberDinner.Application.Authentication.Queries.Login;
+public class LoginQueryValidator : AbstractValidator<LoginQuery>
+{
+    public LoginQueryValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().NotNull();
+        RuleFor(x => x.Password).NotEmpty().NotNull();
+    }
+}
--- BuberDinner.Application/DependencyInjection.cs
+++ BuberDinner.Application/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuberDinner.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace BuberDinner.Application;
@@ -8,6 +11,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
     }
 }
