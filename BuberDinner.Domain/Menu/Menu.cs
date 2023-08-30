ce31c9b9bfd38d93fd38094639f2a850de572f6f
ghostNoté <selcukbilgen@hotmail.com>
2023-08-30 14:18:26 +0300

    Menu aggregate added.

---
+++ BuberDinner.Domain/Menu/Menu.cs
@@ -0,0 +1,13 @@
+﻿using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Menu.Entities;
+using BuberDinner.Domain.Menu.ValueObjects;
+
+namespace BuberDinner.Domain.Menu;
+public sealed class Menu : AggregateRoot<MenuId>
+{
+    private readonly List<MenuSection> _sections = new();
+    public string Name { get; }
+    public string Description { get; }
+    public float AverageRating { get; set; }
+    public IReadOnlyList<MenuSection> Sections => _sections;
+}
